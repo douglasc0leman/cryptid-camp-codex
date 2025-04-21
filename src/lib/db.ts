2c4ec2bac3ddf47e5ce45a39b89a986d999b95bf
Douglas Coleman <dcoleman@mmgapts.com>
2025-04-21 18:02:19 -0400

    added environment variables

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise'
+
+export const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+})
